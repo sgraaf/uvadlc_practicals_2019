a8f7e84ed1934f21a01d93d50c6e388c9009bfa1
sgraaf <sgraaf@users.noreply.github.com>
2019-04-28 20:25:42 +0200

    Update palindrome_length.py

--- assignment_2/part1/palindrome_length.py
+++ assignment_2/part1/palindrome_length.py
@@ -1,13 +1,8 @@
-from multiprocessing.dummy import Pool
-from subprocess import run
+import os
 
-if __name__ == "__main__":
-    model_types = ['RNN', 'LSTM']
-    cmds = [f'python train.py --input_length {i} --model_type {model_type}' for i in range(5, 21) for model_type in model_types]
+model_types = ['RNN', 'LSTM']
+ts = range(5, 21)
 
-    def run_cmd(cmd):
-        run(cmd, shell=True)
-
-    p = Pool(4)
-    p.imap(run_cmd, cmds)
-    p.join()
+for model_type in model_types:
+    for t in ts:
+        os.system(f'python train.py --input_length {t} --model_type {model_type}')
