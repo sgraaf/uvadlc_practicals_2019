0d7f9f9ab4776d8d17094b11611bc74b730929a6
sgraaf <sgraaf@users.noreply.github.com>
2019-04-29 15:52:40 +0200

    Update train.py

--- assignment_2/part2/train.py
+++ assignment_2/part2/train.py
@@ -29,15 +29,19 @@ from torch.utils.data import DataLoader
 
 from dataset import TextDataset
 from model import TextGenerationModel
-from utils import accuracy, one_hot
+from utils import accuracy, create_checkpoint, one_hot, sample_sequence, save_results
 
 ################################################################################
 
 # make the results directory (if it doesn't exist)
-RESULTS_DIR = Path.cwd() / 'results'
+RESULTS_DIR = MODELS_DIR = CHECKPOINTS_DIR = Path.cwd() / 'results'
 RESULTS_DIR.mkdir(parents=True, exist_ok=True)
 
+
 def train(config):
+    # determine the filename (to be used for saving results, checkpoints, models, etc.)
+    filename = Path(config.txt_file).stem
+
     # Initialize the device which to run the model on
     if config.device == 'cuda':
         if torch.cuda.is_available():
@@ -54,11 +58,16 @@ def train(config):
     )
     data_loader = DataLoader(dataset, config.batch_size, num_workers=1)
 
+    # get the vocabulary size and int2char and char2int dictionaries for use later
+    VOCAB_SIZE = dataset.vocab_size
+    CHAR2INT = dataset._char_to_ix
+    INT2CHAR = dataset._ix_to_char
+
     # Initialize the model that we are going to use
     model = TextGenerationModel(
         batch_size=config.batch_size,
         seq_length=config.seq_length,
-        vocabulary_size=dataset.vocab_size,
+        vocabulary_size=VOCAB_SIZE,
         lstm_num_hidden=config.lstm_num_hidden,
         lstm_num_layers=config.lstm_num_layers,
         device=device
@@ -66,9 +75,9 @@ def train(config):
     model.to(device)
 
     # Setup the loss and optimizer
-    criterion =nn.CrossEntropyLoss()
+    criterion = nn.CrossEntropyLoss()
     optimizer = optim.RMSprop(
-        model.parameters(), 
+        model.parameters(),
         config.learning_rate
     )
 
@@ -91,9 +100,9 @@ def train(config):
 
         # convert the data and send to device
         X = torch.stack(batch_inputs, dim=1)
-        X = one_hot(X, dataset.vocab_size)
+        X = one_hot(X, VOCAB_SIZE)
         X = X.to(device)
-        
+
         Y = torch.stack(batch_targets, dim=1)
         Y = Y.to(device)
 
@@ -103,16 +112,17 @@ def train(config):
         # forward pass the mini-batch
         Y_pred, _ = model(X)
 
-        # compute the accuracy and the loss and append them
+        # compute the accuracy and the loss
         accuracy = accuracy(Y_pred, Y)
         loss = criterion.forward(Y_pred.transpose(2, 1), Y)
 
-        results['step'].append(step)
-        results['accuracy'].append(accuracy)
-        results['loss'].append(loss.item())
-        
         # backwards propogate the loss
         loss.backward()
+
+        # clip the gradients (to preven them from exploding)
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=config.max_norm)
+
+        # tune the model parameters
         optimizer.step()
 
         # Just for time measurement
@@ -122,7 +132,15 @@ def train(config):
         if step % config.print_every == 0:
             print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M")}] Train Step {step:04d}/{config.train_steps:04d}, Batch Size = {config.batch_size}, Examples/Sec = {examples_per_second:.2f}, Accuracy = {accuracy:.2f}, Loss = {loss:.3f}')
 
+            # append the accuracy and loss to the results
+            results['step'].append(step)
+            results['accuracy'].append(accuracy)
+            results['loss'].append(loss.item())
+
         if step == config.sample_every:
+            # create a checkpoint
+            create_checkpoint(CHECKPOINTS_DIR, filename, step, model, optimizer, results)
+
             # Generate some sentences by sampling from the model
             pass
 
@@ -131,11 +149,17 @@ def train(config):
             # https://github.com/pytorch/pytorch/pull/9655
             break
 
+    # save the results
+    save_results(RESULTS_DIR, filename, results, model)
+
+    # save the model
+    save_model(MODELS_DIR, filename, model)
+
     print('Done training.')
 
+ ################################################################################
+ ################################################################################
 
- ################################################################################
- ################################################################################
 
 if __name__ == "__main__":
 
@@ -143,28 +167,42 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # Model params
-    parser.add_argument('--txt_file', type=str, required=True, help="Path to a .txt file to train on")
-    parser.add_argument('--seq_length', type=int, default=30, help='Length of an input sequence')
-    parser.add_argument('--lstm_num_hidden', type=int, default=128, help='Number of hidden units in the LSTM')
-    parser.add_argument('--lstm_num_layers', type=int, default=2, help='Number of LSTM layers in the model')
+    parser.add_argument('--txt_file', type=str, required=True,
+                        help="Path to a .txt file to train on")
+    parser.add_argument('--seq_length', type=int, default=30,
+                        help='Length of an input sequence')
+    parser.add_argument('--lstm_num_hidden', type=int,
+                        default=128, help='Number of hidden units in the LSTM')
+    parser.add_argument('--lstm_num_layers', type=int,
+                        default=2, help='Number of LSTM layers in the model')
 
     # Training params
-    parser.add_argument('--batch_size', type=int, default=64, help='Number of examples to process in a batch')
-    parser.add_argument('--learning_rate', type=float, default=2e-3, help='Learning rate')
-    parser.add_argument('--device', type=str, default='cpu', help='Device to use (ie. "cuda" or "cpu")')
+    parser.add_argument('--batch_size', type=int, default=64,
+                        help='Number of examples to process in a batch')
+    parser.add_argument('--learning_rate', type=float,
+                        default=2e-3, help='Learning rate')
+    parser.add_argument('--device', type=str, default='cpu',
+                        help='Device to use (ie. "cuda" or "cpu")')
 
     # It is not necessary to implement the following three params, but it may help training.
-    parser.add_argument('--learning_rate_decay', type=float, default=0.96, help='Learning rate decay fraction')
-    parser.add_argument('--learning_rate_step', type=int, default=5000, help='Learning rate step')
-    parser.add_argument('--dropout_keep_prob', type=float, default=1.0, help='Dropout keep probability')
+    parser.add_argument('--learning_rate_decay', type=float,
+                        default=0.96, help='Learning rate decay fraction')
+    parser.add_argument('--learning_rate_step', type=int,
+                        default=5000, help='Learning rate step')
+    parser.add_argument('--dropout_keep_prob', type=float,
+                        default=1.0, help='Dropout keep probability')
 
-    parser.add_argument('--train_steps', type=int, default=1e6, help='Number of training steps')
+    parser.add_argument('--train_steps', type=int,
+                        default=1e6, help='Number of training steps')
     parser.add_argument('--max_norm', type=float, default=5.0, help='--')
 
     # Misc params
-    parser.add_argument('--summary_path', type=str, default="./summaries/", help='Output path for summaries')
-    parser.add_argument('--print_every', type=int, default=5, help='How often to print training progress')
-    parser.add_argument('--sample_every', type=int, default=100, help='How often to sample from the model')
+    parser.add_argument('--summary_path', type=str,
+                        default="./summaries/", help='Output path for summaries')
+    parser.add_argument('--print_every', type=int, default=5,
+                        help='How often to print training progress')
+    parser.add_argument('--sample_every', type=int, default=100,
+                        help='How often to sample from the model')
 
     config = parser.parse_args()
 
