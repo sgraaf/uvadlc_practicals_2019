90374f5005498d06144b7a3e25a4d23fbe5545ea
sgraaf <sgraaf@users.noreply.github.com>
2019-04-29 10:39:45 +0200

    Update visualize_results.py

--- assignment_2/part1/visualize_results.py
+++ assignment_2/part1/visualize_results.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 plt.style.use('seaborn-white')
@@ -10,30 +11,29 @@ if not RESULTS_DIR.exists():
     raise ValueError('The directory which should contain the results does not exist')
 
 models = ['VanillaRNN', 'LSTM']
+colors = ['tab:orange', 'tab:blue']
 
-for model in models:
+fig, ax1 = plt.subplots()
+
+for i in range(len(models)):
+    model = models[i]
+    color = colors[i]
+
     # open file dialog for the CSV file
     filepath = RESULTS_DIR / (model + '.csv')
 
     df = pd.read_csv(filepath, sep=';')
-    df = df[::20]
+    # print(df)
 
     # create the accuracy-loss curve plots for VanillaRNN
-    fig, ax1 = plt.subplots()
-    ax1.plot(df['Step'], df['Accuracy'], color='tab:orange', marker='.', label='Accuracy')
-    ax1.set_xlabel('Step')
-    ax1.set_ylabel('Accuracy')
-    ax1.tick_params('y')
-    ax1.set_ylim([0, 1])
+    ax1.plot(df['T'], df['Accuracy'], color=color, marker='o', label=model)
 
-    ax2 = ax1.twinx()
-    ax2.plot(df['Step'], df['Loss'], color='tab:blue', marker='.', label='Loss')
-    ax2.set_ylabel('Loss')
-    ax2.tick_params('y')
-    # ax2.set_ylim([0, 5])
+ax1.set_xlabel('T')
+ax1.set_ylabel('Accuracy')
+ax1.set_xticks(np.arange(5, 21))
+ax1.set_yticks(np.arange(0, 1.1, 0.1))
+ax1.set_ylim([0, 1.05])
 
-    h1, l1 = ax1.get_legend_handles_labels()
-    h2, l2 = ax2.get_legend_handles_labels()
-    ax1.legend(h1+h2, l1+l2, loc=0)
-    plt.tight_layout()
-    plt.savefig(RESULTS_DIR / (model + '_accuracy_loss_curves.png'))
+ax1.legend(loc=0)
+plt.tight_layout()
+plt.savefig(RESULTS_DIR / 'palindrome_length_accuracies.png')
