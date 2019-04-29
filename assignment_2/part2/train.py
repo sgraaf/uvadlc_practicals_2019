0458b273f75c1e7b72a1c5eec6232fc45309907a
sgraaf <sgraaf@users.noreply.github.com>
2019-04-29 10:40:04 +0200

    Update train.py

--- assignment_2/part2/train.py
+++ assignment_2/part2/train.py
@@ -13,41 +13,70 @@
 # Date Created: 2018-09-04
 ################################################################################
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+from __future__ import absolute_import, division, print_function
 
+import argparse
 import os
 import time
 from datetime import datetime
-import argparse
+from pathlib import Path
 
 import numpy as np
-
 import torch
+import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 
-from part2.dataset import TextDataset
-from part2.model import TextGenerationModel
+from dataset import TextDataset
+from model import TextGenerationModel
+from utils import accuracy, one_hot
 
 ################################################################################
 
+# make the results directory (if it doesn't exist)
+RESULTS_DIR = Path.cwd() / 'results'
+RESULTS_DIR.mkdir(parents=True, exist_ok=True)
+
 def train(config):
-
     # Initialize the device which to run the model on
-    device = torch.device(config.device)
-
-    # Initialize the model that we are going to use
-    model = TextGenerationModel( ... )  # fixme
+    if config.device == 'cuda':
+        if torch.cuda.is_available():
+            device = torch.device(config.device)
+        else:
+            device = torch.device('cpu')
+    else:
+        device = torch.device(config.device)
 
     # Initialize the dataset and data loader (note the +1)
-    dataset = TextDataset( ... )  # fixme
+    dataset = TextDataset(
+        filename=config.txt_file,
+        seq_length=config.seq_length
+    )
     data_loader = DataLoader(dataset, config.batch_size, num_workers=1)
 
+    # Initialize the model that we are going to use
+    model = TextGenerationModel(
+        batch_size=config.batch_size,
+        seq_length=config.seq_length,
+        vocabulary_size=dataset.vocab_size,
+        lstm_num_hidden=config.lstm_num_hidden,
+        lstm_num_layers=config.lstm_num_layers,
+        device=device
+    )
+    model.to(device)
+
     # Setup the loss and optimizer
-    criterion = None  # fixme
-    optimizer = None  # fixme
+    criterion =nn.CrossEntropyLoss()
+    optimizer = optim.RMSprop(
+        model.parameters(), 
+        config.learning_rate
+    )
+
+    results = {
+        'step': [],
+        'accuracy': [],
+        'loss': []
+    }
 
     for step, (batch_inputs, batch_targets) in enumerate(data_loader):
 
@@ -57,22 +86,41 @@ def train(config):
         #######################################################
         # Add more code here ...
         #######################################################
+        # put the model in training model
+        model.train()
 
-        loss = np.inf   # fixme
-        accuracy = 0.0  # fixme
+        # convert the data and send to device
+        X = torch.stack(batch_inputs, dim=1)
+        X = one_hot(X, dataset.vocab_size)
+        X = X.to(device)
+        
+        Y = torch.stack(batch_targets, dim=1)
+        Y = Y.to(device)
+
+        # (re)set the optimizer gradient to 0
+        optimizer.zero_grad()
+
+        # forward pass the mini-batch
+        Y_pred, _ = model(X)
+
+        # compute the accuracy and the loss and append them
+        accuracy = accuracy(Y_pred, Y)
+        loss = criterion.forward(Y_pred.transpose(2, 1), Y)
+
+        results['step'].append(step)
+        results['accuracy'].append(accuracy)
+        results['loss'].append(loss.item())
+        
+        # backwards propogate the loss
+        loss.backward()
+        optimizer.step()
 
         # Just for time measurement
         t2 = time.time()
         examples_per_second = config.batch_size/float(t2-t1)
 
         if step % config.print_every == 0:
-
-            print("[{}] Train Step {:04d}/{:04d}, Batch Size = {}, Examples/Sec = {:.2f}, "
-                  "Accuracy = {:.2f}, Loss = {:.3f}".format(
-                    datetime.now().strftime("%Y-%m-%d %H:%M"), step,
-                    config.train_steps, config.batch_size, examples_per_second,
-                    accuracy, loss
-            ))
+            print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M")}] Train Step {step:04d}/{config.train_steps:04d}, Batch Size = {config.batch_size}, Examples/Sec = {examples_per_second:.2f}, Accuracy = {accuracy:.2f}, Loss = {loss:.3f}')
 
         if step == config.sample_every:
             # Generate some sentences by sampling from the model
@@ -103,6 +151,7 @@ if __name__ == "__main__":
     # Training params
     parser.add_argument('--batch_size', type=int, default=64, help='Number of examples to process in a batch')
     parser.add_argument('--learning_rate', type=float, default=2e-3, help='Learning rate')
+    parser.add_argument('--device', type=str, default='cpu', help='Device to use (ie. "cuda" or "cpu")')
 
     # It is not necessary to implement the following three params, but it may help training.
     parser.add_argument('--learning_rate_decay', type=float, default=0.96, help='Learning rate decay fraction')
