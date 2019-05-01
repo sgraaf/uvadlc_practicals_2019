7147393875ec653f507072193b799dcb79739e03
sgraaf <sgraaf@users.noreply.github.com>
2019-05-01 16:56:26 +0200

    Update utils.py

--- assignment_2/part2/utils.py
+++ assignment_2/part2/utils.py
@@ -4,6 +4,7 @@ import torch
 from pandas import DataFrame as df
 from pandas import read_csv
 
+WEIRD_CHAR = '\xfe' 
 
 def one_hot(batch, vocab_size, device='cpu'):
     """
@@ -15,13 +16,14 @@ def one_hot(batch, vocab_size, device='cpu'):
     :returns: the one-hot vectors of the batch inputs
     :rtype: torch.Tensor
     """
-    X = torch.zeros(batch.shape, vocab_size, device=device)
+    shape = list(batch.shape) + [vocab_size]
+    X = torch.zeros(shape, device=device)
     X.scatter_(2, batch.unsqueeze(dim=2), 1)
 
     return X
 
 
-def accuracy(predicted_labels, true_labels):
+def get_accuracy(predicted_labels, true_labels):
     """
     Computes the accuracy of the predictions made.
 
@@ -30,8 +32,8 @@ def accuracy(predicted_labels, true_labels):
     :returns: the accuracy
     :rtype: float
     """
-    accuracy = (predicted_labels.argmax(dim=2) == true_labels).sum().float()
-    accuracy /= (predicted_labels.shape[0] * predicted_labels.shape[1])
+    accuracy = (predicted_labels == true_labels.long()).float().mean()
+    # accuracy /= (predicted_labels.shape[0] * predicted_labels.shape[1])
 
     return accuracy
 
@@ -43,15 +45,18 @@ def sample_character(output, temperature=1.0):
     :param torch.Tensor output: the output from the model forward pass
     :param float temperature: the temperature with which to scale the probability distribution
     :returns: the character
-    :rtype: str
+    :rtype: int
     """
-    output = (output.squeeze() / temperature).softmax(dim=0)
-    sample = output.multinomial(dim=1)
+    if temperature is None or temperature == 0.0:
+        sample = output.squeeze().argmax()
+    else:
+        output = (output.squeeze() / temperature).softmax(dim=0)
+        sample = output.multinomial(1)
 
-    return sample.item()
+    return sample
 
 
-def sample_sequence(model, vocab_size, int2char, char2int, T=30, start_char=None, temperature=1.0, device='cpu'):
+def sample_sequence(model, vocab_size, T=30, char=None, temperature=1.0, device='cpu'):
     """
     Sample a sequence of length T, given the previous character.
 
@@ -63,43 +68,35 @@ def sample_sequence(model, vocab_size, int2char, char2int, T=30, start_char=None
     :param str prev_char: the previous character
     :param str device: the device
     :returns: the sampled sequence
-    :rtype: str
+    :rtype: list(int)
     """
     with torch.no_grad():
         # initialize sequence with random character
-        if start_char is None:  # sample a random character and convert to one-hot
-            start_char = torch.randint(vocab_size, (1, 1), dtype=torch.long, device=device)
+        if char is None:  # sample a random character and convert to one-hot
+            char = torch.randint(vocab_size, (1, 1), device=device)
         
-        # convert the start_char to one-hot
-        start_char = one_hot(start_char, vocab_size).to(device)
-        
-        # initialize the sequence to be sampled
-        sequence = [start_char]
+         # initialize the sequence to be sampled
+        sequence = [char[0, 0].item()]
        
-        # run the start_char through the model
-        out, (h, c) = model(start_char)
-        out = out[:, -1, :]
+        # run the char through the model
+        out, h_c = model.forward(char)
         
         # sample the next_char from the model
-        next_char = sample_character(out, temperature)
-        sequence.append(next_char)
+        char[0, 0] = sample_character(out, temperature)
+        sequence.append(char[0, 0].item())
 
-        for t in range(T - 1):
-            # convert the prev_char to one-hot
-            prev_char = one_hot(next_char, vocab_size).to(device)
-
-            # run the prev_char through the model
-            out, (h, c) = model(prev_char, (h, c))
-            out = out[:, -1, :]
-
+        for t in range(2, T):
+            # run the char through the model
+            out, h_c = model.forward(char, h_c)
+            
             # sample the next_char from the model
-            next_char = sample_character(out, temperature)
-            sequence.append(next_char)
+            char[0, 0] = sample_character(out, temperature)
+            sequence.append(char[0, 0].item())
 
         return sequence
 
 
-def create_checkpoint(checkpoint_dir, filename, step, model, optimizer, results):
+def create_checkpoint(checkpoint_dir, filename, step, model, optimizer, results, sequences):
     """
     Creates a checkpoint for the current step
 
@@ -117,7 +114,8 @@ def create_checkpoint(checkpoint_dir, filename, step, model, optimizer, results)
             'step': step,
             'model_state_dict': model.state_dict(),
             'optimizer_state_dict': optimizer.state_dict(),
-            'results': results
+            'results': results,
+            'sequences': sequences
         },
         checkpoint_path
     )
@@ -140,10 +138,10 @@ def load_checkpoint(checkpoint_path, model, optimizer):
     model.load_state_dict(checkpoint['model_state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
     results = checkpoint['results']
-    best_accuracy = checkpoint['best_accuracy']
+    sequences = checkpoint['sequences']
     print('Done!')
 
-    return step, results, best_accuracy
+    return step, results, sequences
 
 
 def save_model(model_dir, filename, model):
@@ -159,7 +157,7 @@ def save_model(model_dir, filename, model):
     print('Done!')
 
 
-def save_results(results_dir, filename, results, model):
+def save_results(results_dir, filename, results, sequences, model):
     """
     Saves the training results
 
@@ -170,5 +168,10 @@ def save_results(results_dir, filename, results, model):
     print('Saving the results...', end=' ')
     results_df = df.from_dict(results)
     results_path = results_dir / f'{model.__class__.__name__}_{filename}_results.csv'
-    results_df.to_csv(results_path, sep=';', encoding='utf-8')
+    results_df.to_csv(results_path, sep=';', encoding='utf-8', index=False)
+
+    sequences_df = df.from_dict(sequences)
+    sequences_df['sequence'] = sequences_df['sequence'].str.replace('\n', WEIRD_CHAR)
+    sequences_path = results_dir / f'{model.__class__.__name__}_{filename}_sequences.csv'
+    sequences_df.to_csv(sequences_path, sep=';', encoding='utf-8', index=False)
     print('Done!')
