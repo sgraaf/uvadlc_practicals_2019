8d410cd61f09d6ba60dbc254673efecb63674e40
sgraaf <sgraaf@users.noreply.github.com>
2019-05-04 19:00:06 +0200

    Update lstm.py

--- assignment_2/part1/lstm.py
+++ assignment_2/part1/lstm.py
@@ -23,6 +23,7 @@ import torch.nn as nn
 
 ################################################################################
 
+
 class LSTM(nn.Module):
 
     def __init__(self, seq_length, input_dim, num_hidden, num_classes, batch_size, device='cpu'):
@@ -34,48 +35,52 @@ class LSTM(nn.Module):
         self.num_classes = num_classes
         self.batch_size = batch_size
         self.device = device
-        
-        mu = 0
-        sigma = 1e-2
 
         # initialize the weights and biases for the input modulation gate
-        self.Wgx = nn.Parameter(mu + sigma * torch.randn((self.input_dim, self.num_hidden), device=self.device))
-        self.Wgh = nn.Parameter(mu + sigma * torch.randn((self.num_hidden, self.num_hidden), device=self.device))
-        self.bg = nn.Parameter(torch.zeros(self.num_hidden, device=self.device))
+        self.Wgx = nn.Parameter(torch.Tensor(self.input_dim, self.num_hidden))
+        self.Wgh = nn.Parameter(torch.Tensor(self.num_hidden, self.num_hidden))
+        self.bg = nn.Parameter(torch.zeros(self.num_hidden))
 
         # initialize the weights and biases for the input gate
-        self.Wix = nn.Parameter(mu + sigma * torch.randn((self.input_dim, self.num_hidden), device=self.device))
-        self.Wih = nn.Parameter(mu + sigma * torch.randn((self.num_hidden, self.num_hidden), device=self.device))
-        self.bi = nn.Parameter(torch.zeros(self.num_hidden, device=self.device))
+        self.Wix = nn.Parameter(torch.Tensor(self.input_dim, self.num_hidden))
+        self.Wih = nn.Parameter(torch.Tensor(self.num_hidden, self.num_hidden))
+        self.bi = nn.Parameter(torch.zeros(self.num_hidden))
 
         # initialize the weights and biases for the forget gate
-        self.Wfx = nn.Parameter(mu + sigma * torch.randn((self.input_dim, self.num_hidden), device=self.device))
-        self.Wfh = nn.Parameter(mu + sigma * torch.randn((self.num_hidden, self.num_hidden), device=self.device))
-        self.bf = nn.Parameter(torch.zeros(self.num_hidden, device=self.device))
+        self.Wfx = nn.Parameter(torch.Tensor(self.input_dim, self.num_hidden))
+        self.Wfh = nn.Parameter(torch.Tensor(self.num_hidden, self.num_hidden))
+        self.bf = nn.Parameter(torch.zeros(self.num_hidden))
 
         # initialize the weights and biases for the output gate
-        self.Wox = nn.Parameter(mu + sigma * torch.randn((self.input_dim, self.num_hidden), device=self.device))
-        self.Woh = nn.Parameter(mu + sigma * torch.randn((self.num_hidden, self.num_hidden), device=self.device))
-        self.bo = nn.Parameter(torch.zeros(self.num_hidden, device=self.device))
+        self.Wox = nn.Parameter(torch.Tensor(self.input_dim, self.num_hidden))
+        self.Woh = nn.Parameter(torch.Tensor(self.num_hidden, self.num_hidden))
+        self.bo = nn.Parameter(torch.zeros(self.num_hidden))
 
         # intialize the weights and biases for the output
-        self.Wph = nn.Parameter(mu + sigma * torch.randn((self.num_hidden, self.num_classes), device=self.device))
-        self.bp = nn.Parameter(torch.zeros(self.num_classes, device=self.device))
+        self.Wph = nn.Parameter(torch.Tensor(self.num_hidden, self.num_classes))
+        self.bp = nn.Parameter(torch.zeros(self.num_classes))
+
+        # actually initialize the weights
+        for parameter in [self.Wgx, self.Wgh, self.Wix, self.Wih, self.Wfx, self.Wfh, self.Wox, self.Woh, self.Wph]:
+            torch.nn.init.xavier_normal_(parameter)
 
         # initialize the activation functions
         self.tanh = nn.Tanh()
         self.sigmoid = nn.Sigmoid()
+
+        # send to device
+        self.to(device)
 
     def forward(self, x):
         # Implementation here ...
         # initialize the hidden state(s)
         h = torch.zeros((self.batch_size, self.num_hidden), device=self.device)
         c = torch.zeros((self.batch_size, self.num_hidden), device=self.device)
-        
+
         # compute the hidden state
         for i in range(self.seq_length):
             xi = x[:, i].view(self.batch_size, self.input_dim)
-            
+
             g = self.tanh(
                 xi @ self.Wgx +
                 h @ self.Wgh +
