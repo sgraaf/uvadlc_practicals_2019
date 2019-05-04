687fa34fcc606c391b4a9a74fc0ce45fded17996
sgraaf <sgraaf@users.noreply.github.com>
2019-05-04 19:00:15 +0200

    Update visualize_results.py

--- assignment_2/part1/visualize_results.py
+++ assignment_2/part1/visualize_results.py
@@ -3,37 +3,107 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
+from PIL import Image, ImageChops, ImageOps
 
-plt.style.use('seaborn-white')
+sns.set_palette('cubehelix')
+
+
+def trim(image):
+    background = Image.new(image.mode, image.size, image.getpixel((0, 0)))
+    diff = ImageChops.difference(image, background)
+    diff = ImageChops.add(diff, diff, 2.0, -100)
+    bbox = diff.getbbox()
+    if bbox:
+        return image.crop(bbox)
+
+
+def pad(image):
+    return ImageOps.expand(image, border=5, fill='white')
+
 
 RESULTS_DIR = Path.cwd() / 'results'
 if not RESULTS_DIR.exists():
-    raise ValueError('The directory which should contain the results does not exist')
+    raise ValueError(
+        'The directory which should contain the results does not exist')
 
 models = ['VanillaRNN', 'LSTM']
-colors = ['tab:orange', 'tab:blue']
-
-fig, ax1 = plt.subplots()
 
 for i in range(len(models)):
     model = models[i]
-    color = colors[i]
 
     # open file dialog for the CSV file
     filepath = RESULTS_DIR / (model + '.csv')
 
-    df = pd.read_csv(filepath, sep=';')
+    df = pd.read_csv(filepath, sep=';', encoding='utf-8')
     # print(df)
 
-    # create the accuracy-loss curve plots for VanillaRNN
-    ax1.plot(df['T'], df['Accuracy'], color=color, marker='o', label=model)
+    # plot accuracy over timesteps for all palindrome lengths
+    df_ = df
+    df_ = df_[(df_['T'] % 2 == 1)]  # filter out certain T-values
+    df_['Step'] = df_['Step'].apply(
+        lambda x: x - x % 250)  # group rows by step
+    fig, ax = plt.subplots(1, 1)
+    sns_plot = sns.lineplot(x='Step', y='Accuracy',
+                            hue='T', data=df_, legend='full', ax=ax)
+    box = sns_plot.get_position()
+    sns_plot.set_position([box.x0, box.y0, box.width * 0.85, box.height])
+    sns_plot.legend(loc='center right', bbox_to_anchor=(1.25, 0.5), ncol=1)
+    plt.savefig(RESULTS_DIR / f'{model}_accuracy_steps_odd.png')
+    plt.close()
 
-ax1.set_xlabel('T')
-ax1.set_ylabel('Accuracy')
-ax1.set_xticks(np.arange(5, 21))
-ax1.set_yticks(np.arange(0, 1.1, 0.1))
-ax1.set_ylim([0, 1.05])
+    # plot loss over timesteps for all palindrome lengths
+    fig, ax = plt.subplots(1, 1)
+    sns_plot = sns.lineplot(x='Step', y='Loss', hue='T',
+                            data=df_, legend='full', ax=ax)
+    box = sns_plot.get_position()
+    sns_plot.set_position([box.x0, box.y0, box.width * 0.85, box.height])
+    sns_plot.legend(loc='center right', bbox_to_anchor=(1.25, 0.5), ncol=1)
+    plt.savefig(RESULTS_DIR / f'{model}_loss_steps_odd.png')
+    plt.close()
 
-ax1.legend(loc=0)
-plt.tight_layout()
-plt.savefig(RESULTS_DIR / 'palindrome_length_accuracies.png')
+    df_ = df
+    df_ = df_[(df_['T'] % 2 == 0)]  # filter out certain T-values
+    df_['Step'] = df_['Step'].apply(
+        lambda x: x - x % 250)  # group rows by step
+    fig, ax = plt.subplots(1, 1)
+    sns_plot = sns.lineplot(x='Step', y='Accuracy',
+                            hue='T', data=df_, legend='full', ax=ax)
+    box = sns_plot.get_position()
+    sns_plot.set_position([box.x0, box.y0, box.width * 0.85, box.height])
+    sns_plot.legend(loc='center right', bbox_to_anchor=(1.25, 0.5), ncol=1)
+    plt.savefig(RESULTS_DIR / f'{model}_accuracy_steps_even.png')
+    plt.close()
+
+    # plot loss over timesteps for all palindrome lengths
+    fig, ax = plt.subplots(1, 1)
+    sns_plot = sns.lineplot(x='Step', y='Loss', hue='T',
+                            data=df_, legend='full', ax=ax)
+    box = sns_plot.get_position()
+    sns_plot.set_position([box.x0, box.y0, box.width * 0.85, box.height])
+    sns_plot.legend(loc='center right', bbox_to_anchor=(1.25, 0.5), ncol=1)
+    plt.savefig(RESULTS_DIR / f'{model}_loss_steps_even.png')
+    plt.close()
+
+    # plot accuracy over timesteps for all palindrome lengths
+    df_ = df
+    fig, ax = plt.subplots(1, 1)
+    sns_plot = sns.lineplot(x='T', y='Accuracy', data=df_, ax=ax)
+    plt.tight_layout()
+    plt.savefig(RESULTS_DIR / f'{model}_accuracy_lengths.png')
+    plt.close()
+
+    # plot loss over timesteps for all palindrome lengths
+    fig, ax = plt.subplots(1, 1)
+    sns_plot = sns.lineplot(x='T', y='Loss', data=df_, ax=ax)
+    plt.tight_layout()
+    plt.savefig(RESULTS_DIR / f'{model}_loss_lengths.png')
+    plt.close()
+
+# trim images
+images_file_paths = RESULTS_DIR.glob('*.png')
+for images_file_path in images_file_paths:
+	I = Image.open(images_file_path)
+	I = trim(I)
+	I = pad(I)
+	I.save(images_file_path.parent / (images_file_path.stem + '_trimmed' + images_file_path.suffix))
